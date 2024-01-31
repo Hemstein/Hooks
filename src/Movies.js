0bc6229bc0b139baeb663f629f5b52c7d4ce8b5f
Hemstein <haythem_benjemaa@hotmail.de>
2024-01-31 16:18:42 +0100

    hooks

---
+++ src/Movies.js
@@ -0,0 +1,45 @@
+
+
+const movies = [
+    {
+        id:0,
+        "title": "Kingsglaive",
+        "description": "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
+        "posterUrl": "https://m.media-amazon.com/images/I/91WGW1i0OZL._AC_UF894,1000_QL80_.jpg",
+        "rating": 4,
+        "trailerSrc": "https://www.youtube.com/embed/htnkOpknGok"
+    },
+    {
+        id:1,
+        "title": "Final Fantasy",
+        "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
+        "posterUrl": "https://cdn11.bigcommerce.com/s-6rs11v9w2d/images/stencil/1280x1280/products/3054/16401/FFVII_RB_AG_US__14845.1702581565.jpg?c=1",
+        "rating": 1,
+        "trailerSrc": "https://www.youtube.com/embed/4tyuIh12_HU"
+    },
+    {
+        id:2,
+        "title": "Ghost In The Shell",
+        "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
+        "posterUrl": "https://m.media-amazon.com/images/M/MV5BMzJiNTI3MjItMGJiMy00YzA1LTg2MTItZmE1ZmRhOWQ0NGY1XkEyXkFqcGdeQXVyOTk4MTM0NQ@@._V1_.jpg",
+        "rating": 5,
+        "trailerSrc": "https://www.youtube.com/embed/G4VmJcZR0Yg"
+    },
+    {
+        id:3,
+        "title": "Appleseed Alpha",
+        "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope.",
+        "posterUrl": "https://m.media-amazon.com/images/M/MV5BMTgzMDY2Njc4Nl5BMl5BanBnXkFtZTgwNTEyMjUzMjE@._V1_.jpg",
+        "rating": 4,
+        "trailerSrc": "https://www.youtube.com/embed/4DtWCjogd1M"
+    },
+    {
+        id:4,
+        "title": "Resident Evil",
+        "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
+        "posterUrl": "https://m.media-amazon.com/images/M/MV5BZmI1ZGRhNDYtOGVjZC00MmUyLThlNTktMTQyZGE3MzE1ZTdlXkEyXkFqcGdeQXVyNDE5MTU2MDE@._V1_FMjpg_UX1000_.jpg",
+        "rating": 2,
+        "trailerSrc": "https://www.youtube.com/embed/W1OUs3HwIuo"
+    }
+];
+export default movies;
